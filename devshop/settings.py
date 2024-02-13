18fd1ea52452bc0ca9fcb4ffdd7f959bf40d3992
Nazmul <technazmul.hoque@gmail.com>
2024-02-13 03:07:46 -0500

    Added migrations dir

--- devshop/settings.py
+++ devshop/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -81,9 +81,16 @@ WSGI_APPLICATION = 'devshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'devshop',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': os.environ['DB_PASSWORD']
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
