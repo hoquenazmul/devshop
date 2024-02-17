eaefa49010646b2919778ae70066e40daa4dc0c8
Nazmul <technazmul.hoque@gmail.com>
2024-02-17 18:37:20 -0500

    Added computed value for inventory col

--- store/admin.py
+++ store/admin.py
@@ -4,9 +4,16 @@ from . import models
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price']
+    list_display = ['title', 'unit_price', 'inventory_status']
     list_editable = ['unit_price']
     list_per_page = 10
+    
+    # adding computed column
+    @admin.display(ordering='inventory') # sorting computed coloumn
+    def inventory_status(self, product):
+        if product.inventory < 10:
+            return 'Low'
+        return 'OK'
     
     
 @admin.register(models.Customer)
