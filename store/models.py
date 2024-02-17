cd04f4e70a2efa4f8ffb627c7802f39a8d11469b
Nazmul <technazmul.hoque@gmail.com>
2024-02-17 18:34:44 -0500

    Added string rep method for product model and ordered by title

--- store/models.py
+++ store/models.py
@@ -33,6 +33,12 @@ class Collection(models.Model):
     # Circular Relationship
     feature_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='+')
     
+    def __str__(self) -> str:
+        return self.title
+    
+    class Meta:
+        ordering = ['title']
+    
 
 class Promotion(models.Model):
     description = models.CharField(max_length=255)
@@ -48,6 +54,12 @@ class Product(models.Model):
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
     promotions = models.ManyToManyField(Promotion)
+        
+    def __str__(self) -> str:
+        return self.title
+    
+    class Meta:
+        ordering = ['title']
 
 
 class Order(models.Model):
