7867d7342ce105e7fc92cbc4716c008126fea802
Nazmul <technazmul.hoque@gmail.com>
2024-02-17 23:44:31 -0500

    Added string rep for customer model and ordered by first, last anem

--- store/models.py
+++ store/models.py
@@ -18,6 +18,12 @@ class Customer(models.Model):
     phone = models.CharField(max_length=20)
     birth_date = models.DateField(null=True)
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+    
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
+    
+    class Meta:
+        ordering = ['first_name', 'last_name']
 
 
 class Address(models.Model):
