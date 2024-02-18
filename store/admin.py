f7167b0dfcb391af667611751652daae792ad870
Nazmul <technazmul.hoque@gmail.com>
2024-02-17 19:41:45 -0500

    Displayed product related collection object title

--- store/admin.py
+++ store/admin.py
@@ -4,9 +4,13 @@ from . import models
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price', 'inventory_status']
+    list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
     list_editable = ['unit_price']
     list_per_page = 10
+    list_select_related = ['collection']
+    
+    def collection_title(self, product):
+        return product.collection.title
     
     # adding computed column
     @admin.display(ordering='inventory') # sorting computed coloumn
