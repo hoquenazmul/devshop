6f1c01f6d6ddd451c5bf6d3821dde31a7372bc9d
Nazmul <technazmul.hoque@gmail.com>
2024-02-18 02:19:20 -0500

    Implemented customer searching functionality

--- store/admin.py
+++ store/admin.py
@@ -58,10 +58,11 @@ class OrderAdmin(admin.ModelAdmin):
     
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ['id', 'first_name', 'last_name', 'membership', 'orders_count']
+    list_display = ['first_name', 'last_name', 'membership', 'orders_count']
     list_editable = ['membership']
-    ordering = ['first_name', 'last_name']
     list_per_page = 10
+    ordering = ['first_name', 'last_name']
+    search_fields = ['first_name__istartswith', 'last_name__istartswith']
     
     @admin.display(ordering='orders_count')
     def orders_count(self, customer):
