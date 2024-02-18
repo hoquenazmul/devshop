99b08ccf72b12c352a98584db7ea262da8644762
Nazmul <technazmul.hoque@gmail.com>
2024-02-18 02:27:55 -0500

    Implemented filter for product

--- store/admin.py
+++ store/admin.py
@@ -35,6 +35,7 @@ class CollectionAdmin(admin.ModelAdmin):
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
     list_editable = ['unit_price']
+    list_filter = ['collection', 'last_update']
     list_per_page = 10
     list_select_related = ['collection']
     
