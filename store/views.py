e7918e77ea464881ef54054fe426528983297d87
Nazmul <technazmul.hoque@gmail.com>
2024-02-19 11:03:35 -0500

    Showed 404 if object not exists

--- store/views.py
+++ store/views.py
@@ -1,5 +1,7 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Product
 from .serializers import ProductSerializer
 
@@ -11,7 +13,15 @@ def product_list(request):
 
 @api_view()
 def product_detail(request, id):
-    product = Product.objects.get(pk=id)
+    product = get_object_or_404(Product, pk=id) # return 404 if object not exists
     serializer = ProductSerializer(product)
     return Response(serializer.data)
+
+    # another way to handle 404 if object not exists
+    # try:
+    #     product = Product.objects.get(pk=id)
+    #     serializer = ProductSerializer(product)
+    #     return Response(serializer.data)
+    # except Product.DoesNotExist:
+    #     return Response(status=status.HTTP_404_NOT_FOUND)
     
