9e8d4fbe7c57226b91b59799174e4ba8229d372e
Nazmul <technazmul.hoque@gmail.com>
2024-02-19 13:35:08 -0500

    Replaced ModelSerializer by Serializer class to avoid duplicate fields value between model and serializer

--- store/serializers.py
+++ store/serializers.py
@@ -3,17 +3,18 @@ from rest_framework import serializers
 from .models import Product, Collection
 
 
-class CollectionSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id', 'title']
 
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'unit_price', 'price_with_tax', 'collection']
+    
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
-    collection = CollectionSerializer()
     
     def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
