59e7d1be363efb8fd4237244f41102f6be4e1bea
Nazmul <technazmul.hoque@gmail.com>
2024-02-19 17:29:49 -0500

    Implemented CRUD operations for collection object

--- store/urls.py
+++ store/urls.py
@@ -5,5 +5,7 @@ from . import views
 # URLConf
 urlpatterns = [
     path("products/", views.product_list),
-    path("products/<int:id>/", views.product_detail), # will accept only int type id
+    path("products/<int:id>/", views.product_detail),
+    path("collections/", views.colloection_list),
+    path("collections/<int:pk>/", views.colloection_detail)
 ]
--- store/views.py
+++ store/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import get_object_or_404
+from django.db.models import Count
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Product
-from .serializers import ProductSerializer
+from .models import Product, Collection
+from .serializers import ProductSerializer, CollectionSerializer
 
 
 @api_view(['GET', 'POST'])
@@ -37,5 +38,39 @@ def product_detail(request, id):
             return Response({'error': 'Product cannot be deleted because it is associated with an order item'}, 
                             status=status.HTTP_405_METHOD_NOT_ALLOWED)
         product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+
+@api_view(['GET', 'POST'])
+def colloection_list(request):
+    if request.method == 'GET':
+        queryset = Collection.objects.annotate(products_count=Count('product__id')).all()
+        serializer = CollectionSerializer(queryset, many=True)
+        return Response(serializer.data)
+    if request.method == 'POST':
+        serializer = CollectionSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    
+@api_view(['GET', 'PUT', 'DELETE'])
+def colloection_detail(request, pk):
+    collection = get_object_or_404(
+        Collection.objects.annotate(products_count=Count('product')), pk=pk)
+    
+    if request.method == 'GET':
+        serializer = CollectionSerializer(collection)
+        return Response(serializer.data)
+    if request.method == 'PUT':
+        serializer = CollectionSerializer(collection, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    if request.method == 'DELETE':
+        if collection.product_set.count() > 0:
+            return Response({'error': 'Collection cannot be deleted because it has one or more products'}, 
+                            status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        collection.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
     
