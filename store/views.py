a181a1fb323e8591b7d4fddcf46f9690b65298aa
Nazmul <technazmul.hoque@gmail.com>
2024-02-21 07:14:08 -0500

    Retrived product_pk from url and Displayed reviews based on product id

--- store/views.py
+++ store/views.py
@@ -2,8 +2,8 @@ from django.db.models import Count
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import status
-from .models import Product, Collection, OrderItem
-from .serializers import ProductSerializer, CollectionSerializer
+from .models import Product, Collection, OrderItem, Review
+from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer
 
 
 class ProductViewSet(ModelViewSet):
@@ -29,4 +29,15 @@ class CollectionViewSet(ModelViewSet):
             return Response({'error': 'Collection cannot be deleted because it has one or more products'}, 
                             status=status.HTTP_405_METHOD_NOT_ALLOWED)
         return super().destroy(request, *args, **kwargs)
+    
+
+class ReviewViewSet(ModelViewSet):
+    serializer_class = ReviewSerializer
+    
+    def get_queryset(self):
+        return Review.objects.filter(product_id=self.kwargs['product_pk'])
+    
+    def get_serializer_context(self):
+        # print(self.kwargs)
+        return {'product_id': self.kwargs['product_pk']}
     
