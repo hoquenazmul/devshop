52d541492196913b64a0f49a7e3defce8d18d79f
Nazmul <technazmul.hoque@gmail.com>
2024-02-26 06:15:26 -0500

    Added custom permission

--- store/models.py
+++ store/models.py
@@ -95,6 +95,11 @@ class Order(models.Model):
     placed_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+    
+    class Meta:
+        permissions = [
+            ('cancel_order', 'Can cancel order')
+        ]
 
 
 class OrderItem(models.Model):
