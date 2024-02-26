215d449aed33992f277dd9dabc0ceb1b49014e32
Nazmul <technazmul.hoque@gmail.com>
2024-02-25 19:34:23 -0500

    Extended AbstractUser model and set custom User in settings module for authentication

--- devshop/settings.py
+++ devshop/settings.py
@@ -136,6 +136,8 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
+AUTH_USER_MODEL = 'core.User'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 INTERNAL_IPS = [
