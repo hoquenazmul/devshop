65bb5b737b42445de20ceb8ba7a96f76b535a317
Nazmul <technazmul.hoque@gmail.com>
2024-03-05 10:32:59 -0500

    Returned created order after saving order & order items

--- store/views.py
+++ store/views.py
@@ -98,6 +98,14 @@ class CustomerViewSet(ModelViewSet):
 class OrderViewSet(ModelViewSet):
     permission_classes = [IsAuthenticated]
     
+    def create(self, request, *args, **kwargs):
+        serializer = CreateOrderSerializer(data=request.data, 
+                                           context={'user_id': self.request.user.id})
+        serializer.is_valid(raise_exception=True)
+        order = serializer.save()
+        serializer = OrderSerializer(order)
+        return Response(serializer.data)
+    
     def get_queryset(self):
         user = self.request.user
         
@@ -111,6 +119,3 @@ class OrderViewSet(ModelViewSet):
         if self.request.method == 'POST':
             return CreateOrderSerializer
         return OrderSerializer
-    
-    def get_serializer_context(self):
-        return {'user_id': self.request.user.id}
