df76fde5ad28911013d99837e101a683a1def0b4
Nazmul <technazmul.hoque@gmail.com>
2024-03-06 09:27:44 -0500

    Added images field to products endpoint

--- store/views.py
+++ store/views.py
@@ -15,7 +15,7 @@ from .permissions import IsAdminOrReadOnly
 
 
 class ProductViewSet(ModelViewSet):
-    queryset = Product.objects.all()
+    queryset = Product.objects.prefetch_related('images').all()
     serializer_class = ProductSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = ProductFilter
