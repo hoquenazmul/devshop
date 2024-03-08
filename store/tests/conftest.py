3f902a03099eb3adc2de9cb026ce0fefa79c1475
Nazmul <technazmul.hoque@gmail.com>
2024-03-08 13:47:32 -0500

    Implemented authenticate fixture to handle authentication

--- store/tests/conftest.py
+++ store/tests/conftest.py
@@ -5,4 +5,14 @@ import pytest
 def api_client():
     from rest_framework.test import APIClient
     
-    return APIClient() 
+    return APIClient()
+
+
+@pytest.fixture
+def authenticate(api_client):
+    from django.contrib.auth.models import User
+    
+    def do_authenticate(is_staff=False):
+        return api_client.force_authenticate(user=User(is_staff=is_staff))
+    
+    return do_authenticate
--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -19,23 +19,23 @@ class TestCreateCollection:
         
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
         
-    def test_if_user_is_not_admin_returns_403(self, api_client, create_collection):
-        api_client.force_authenticate(user={})
+    def test_if_user_is_not_admin_returns_403(self, authenticate, create_collection):
+        authenticate(is_staff=False)
         
         response = create_collection({'title': 'a'})
         
         assert response.status_code == status.HTTP_403_FORBIDDEN
         
-    def test_if_data_is_invalid_returns_400(self, api_client, create_collection):
-        api_client.force_authenticate(user=User(is_staff=True))
+    def test_if_data_is_invalid_returns_400(self, authenticate, create_collection):
+        authenticate(is_staff=True)
         
         response = create_collection({'title': ''})
         
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert response.data['title'] is not None
 
-    def test_if_data_is_valid_returns_201(self, api_client, create_collection):
-        api_client.force_authenticate(user=User(is_staff=True))
+    def test_if_data_is_valid_returns_201(self, authenticate, create_collection):
+        authenticate(is_staff=True)
         
         response = create_collection({'title': 'collection title'})
         
