ce6b596be645a57a852b4d54f597a044c5dd89e9
Nazmul <technazmul.hoque@gmail.com>
2024-03-07 23:57:16 -0500

    Added email configuration

--- devshop/settings.py
+++ devshop/settings.py
@@ -174,3 +174,14 @@ DJOSER = {
         'current_user': 'core.serializers.UserSerializer'
     }
 }
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = 'localhost'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 2525
+DEFAULT_FROM_EMAIL= 'info@devshop.com'
+
+ADMINS = [
+    ('admin', 'admin@devshop.com')
+]
