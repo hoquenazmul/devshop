548f4d4d499778dc55a9a905f1d5dc085f9ce147
Nazmul <technazmul.hoque@gmail.com>
2024-05-30 22:45:46 -0400

    testing

--- likes/models.py
+++ likes/models.py
@@ -9,3 +9,5 @@ class LikedItem(models.Model):
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey()
+
+testing1....
